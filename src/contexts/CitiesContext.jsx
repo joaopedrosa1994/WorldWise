93df04d76e3eac7d853c27e2d991ec401c39b230
João Pedro Santos Assunção <joaopedrosantosassuncao@gmail.com>
2023-12-14 12:51:23 -0300

    updated app

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -8,7 +8,7 @@ import {
 
 const CitiesContext = createContext();
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://db-maps.onrender.com';
 
 const initialState = {
   cities: [],
